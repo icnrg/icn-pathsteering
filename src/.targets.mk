8c5264ba2e92c8ff929c2d6aef6e24e20c98a86e
David Oran <daveoran@orandom.net>
2019-09-24 12:45:14 -0400

    Fix merge problems

--- src/.targets.mk
+++ src/.targets.mk
@@ -1,4 +1,2 @@
 TARGETS_DRAFTS := draft-oran-icnrg-pathsteering
 TARGETS_TAGS := 
-draft-oran-icnrg-pathsteering-00.xml: draft-oran-icnrg-pathsteering.xml
-	sed -e 's/draft-oran-icnrg-pathsteering-latest/draft-oran-icnrg-pathsteering-00/g' $< >$@
